a1ebff06fa63dff5ad554c1697c825d75dcccda0
sahil-develops <zambanisahil99@gmail.com>
2025-02-12 22:10:12 +0530

    Add react-markdown dependency and update NewBlogPage to use Blogs component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,11 @@
 // src/app/admin/blog/new/page.tsx
 'use client';
 
-import { BlogEditor } from '@/app/components/Blog/BlogEditor/index';
+// import { BlogEditor } from '@/app/components/Blog/BlogEditor/index';
 
 import { BlogFormValues } from '@/app/components/Blog/types';
+import { BlogEditorToolbar } from '../components/Blog/BlogEditor/BlogEditorToolbar';
+import Blogs from '../components/Blog/BlogEditor';
 export default function NewBlogPage() {
   const handleSave = async (data: BlogFormValues) => {
     console.log('Saving blog post:', data); // Ensure this logs correctly
@@ -18,7 +20,7 @@ export default function NewBlogPage() {
 
   return (
     <div className="container mx-auto py-10">
-      <BlogEditor onSave={handleSave} />
+  <Blogs/>
     </div>
   );
 }
